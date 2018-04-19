fe61ab5055584d3e9abbdecce1f596114be9781c
Unknown <francis.tremblay1990@gmail.com>
2018-04-18 20:55:20 -0400

    Adds gamemanager, player controller and player movement state scripts

---
+++ Assets/_Scripts/Players/Controllers/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using Assets._Scripts.Players.States;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace Assets._Scripts.Players.Controllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public PlayerMovementState MovementState;
+
+        public void FixedUpdate()
+        {
+            float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+            float vertical = CrossPlatformInputManager.GetAxis("Vertical");
+            Vector3 move = vertical * Vector3.forward + horizontal * Vector3.right;
+            Debug.Log(string.Format("({0}, {1})", horizontal, vertical));
+        }
+    }
+}
---
+++ Assets/_Scripts/Players/States/PlayerMovementState.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Assets._Scripts.Players.States
+{
+    [CreateAssetMenu(fileName = "Player Movement State", menuName = "States/Player Movement")]
+    public class PlayerMovementState : ScriptableObject
+    {
+
+    }
+}
