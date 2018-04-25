0fb8eabc9d0a27dff62f21f76961b5dc822f398d
Unknown <francis.tremblay1990@gmail.com>
2018-04-24 21:12:23 -0400

    Player Movement
    Adds player movement state, player controller and player view

--- Assets/_Scripts/Players/States/PlayerMovementState.cs
+++ Assets/_Scripts/Players/States/PlayerMovementState.cs
@@ -5,6 +5,13 @@ namespace Assets._Scripts.Players.States
     [CreateAssetMenu(fileName = "Player Movement State", menuName = "States/Player Movement")]
     public class PlayerMovementState : ScriptableObject
     {
+        public Vector3 MovementDirection;
 
+        public float MovementSpeed = 10;
+        public float TurnSpeed = 10;
+
+        public float Drag = 10;
+        public float AngularDrag = 0.8f;
     }
+
 }
