866e5b6e8a18a75f4acbb8829d7067b19ac81ae0
Dave Gagné <davegagne.creative@gmail.com>
2018-09-17 22:37:19 -0400

    !V UI Rotation script created and added in Dave's scene

---
+++ Assets/_Scripts/UI Scripts/UiCircleScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiCircleScript : MonoBehaviour {
+
+    Image circleUi;
+
+    public float rotationSpeed = 35f;
+
+    public Transform playerObject;
+    public Transform target;
+
+    void Update() {
+        //target.transform.position = playerObject.transform.position;
+        target.transform.position = new Vector3(playerObject.transform.position.x,
+            playerObject.transform.position.y + 0.2f, playerObject.transform.position.z);
+
+
+        transform.Rotate(0, 0, Time.deltaTime * rotationSpeed);
+    }
+
+    void Start() {
+        circleUi = GetComponent<Image>();
+        var tempColor = circleUi.color;
+        tempColor.a = 0.65f;
+        circleUi.color = tempColor;
+    }
+
+} //class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
