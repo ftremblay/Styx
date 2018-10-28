22b437bbc60427970efac1032387d756b9153c23
Unknown <francis.tremblay1990@gmail.com>
2018-10-28 13:29:43 -0400

    Uses invoke to reactivate puck collider after pass
    To be sure that the puck cannot be grabbed by the same player again

--- Assets/_Scripts/States/Puck/PuckCarriedState.cs
+++ Assets/_Scripts/States/Puck/PuckCarriedState.cs
@@ -1,12 +1,27 @@
 ﻿using RageCure.StateUtils;
 using Styx.Entities.PuckModule;
+using UnityEngine;
 
 namespace Styx.States
 {
     public class PuckCarriedState : State<PuckState>
     {
+        [SerializeField]
+        private SphereCollider _collider;
+
+        private void ReactivateCollider()
+        {
+            _collider.enabled = true;
+        }
+
+        public void Start()
+        {
+            _collider = _collider ?? GetComponent<SphereCollider>();
+        }
+
         public override void Enter(PuckState entity)
         {
+            _collider.enabled = false;
         }
 
         public override void Execute(PuckState entity)
@@ -15,6 +30,7 @@ namespace Styx.States
 
         public override void Exit(PuckState entity)
         {
+            Invoke("ReactivateCollider", 0.2f);
         }
 
         public override void FixedExecute(PuckState entity)
